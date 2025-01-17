d086c0ec68f19888acd13e620851dedd5f603c43
Alan Chambers <ajmchambers@users.noreply.github.com>
2025-01-17 11:05:05 +0000

    Export additional event types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export type {
   BroadcastChannelStorageOptions,
+  BroadcastChannelReadyEvent,
+  BroadcastChannelClosedEvent,
+  BroadcastChannelChangeEvent,
   BroadcastChannelStorageEvent,
 } from './BroadcastChannelStorage.js';
 export { BroadcastChannelStorage } from './BroadcastChannelStorage.js';
